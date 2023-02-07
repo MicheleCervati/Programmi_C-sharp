493abc63cca72828b693d17aa8087c95c1773df5
Michele Cervati <121225803+MicheleCervati@users.noreply.github.com>
2023-02-07 09:24:38 +0100

    es 07/02

---
+++ cifreRandom/cifreRandom/cifreRandom/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace cifreRandom
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random casuale = new Random();
+            int[] numeri = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int numCasuale;
+            string numNonUsciti = "";
+
+            for (int i = 0; i < 10; i++)
+            {
+                numCasuale = casuale.Next(0, 10);
+                Console.WriteLine(numCasuale);
+                for (int j = 0; j < numeri.Length; j++)
+                {
+                    if (numCasuale == numeri[j])
+                    {
+                        numeri[j] = -1;
+                    }
+                }
+            }
+
+            for (int p = 0; p < numeri.Length; p++)
+            {
+                Console.WriteLine("ciao " + numeri[p]);
+                if (numeri[p] != -1)
+                {
+                    numNonUsciti = numNonUsciti + numeri[p];
+                }
+            }
+
+            Console.WriteLine("I numeri non usciti sono: ");
+            Console.WriteLine(numNonUsciti);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ cifreRandom/cifreRandom_2.0/cifreRandom_2.0/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace cifreRandom_2._0
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            int[] numeri = new int[10];
+            Random causale = new Random();
+            int numEstratto;
+
+            for (int i = 0; i < 10; i++)
+            {
+                numEstratto = causale.Next(0, 10);
+                numeri[numEstratto] = -1;
+                Console.WriteLine("Il numero estratto è " + numEstratto);
+            }
+
+            for (int j = 0; j < 10; j++)
+            {
+                if (numeri[j] == -1)
+                {
+                    Console.WriteLine("Non è uscito il numero: " + j);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
