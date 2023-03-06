1569aae9bb0531037469969d07083ca74290f69e
MicheleCervati <michele.cervati@iisviolamarchesini.edu.it>
2023-03-06 23:59:36 +0100

    Commit di esercizio Parola e FrequenzaCaratteri in una Frase

---
+++ FrequenzaCaratteriInUnaFrase_CervatiMichele/FrequenzaCaratteriInUnaFrase_CervatiMichele/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrequenzaCaratteriInUnaFrase_CervatiMichele
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizzare un programma che legga una frase con Console.Read() e che permetta(utilizzando un vettore)
+            //di determinare la frequenza dei caratteri alfabetici, il conteggio totale delle cifre e il conteggio totale
+            //di tutti gli altri caratteri.
+            //NOTA: ipotizzare che l'utente inserisca solo caratteri alfabetici maiuscoli.
+            
+            char carattere;
+            int[] frequenza = new int[94]; //94 è il numero dei caratteri stampabili più comuni che vanno da ASCII 32 a 126 
+            string [] tabellaAscii = new string[] {" " , "!" , "'" , "#" , "$" , "%" , "&" , "'" , "(" , ")" , "*" , "+" , "," , "-" , "." , "/" , "0" , "1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , ":" , "," , "" , "=" , ">" , "?" , "@" , "A" , "B" , "C" , "D" , "E" , "F" , "G" , "H" , "I" , "J" , "K" , "L" , "M" , "N" , "O" , "P" , "Q" , "R" , "S" , "T" , "U" , "V" , "W" , "X" , "Y" , "Z" , "[" , "/" , "]" , "^" , "_" , "`" , "a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z" , "{" , "|" , "}" , "~"} ;
+
+
+
+            Console.WriteLine("Inserisci una frase: ");
+            do
+            {
+                carattere = Convert.ToChar(Console.Read()); 
+                if (carattere >= 32 && carattere <= 126) //controllo se il carattere è nell'internavllo gestito dal programma
+                {
+                    frequenza[carattere - 32] = frequenza[carattere - 32] + 1; //se si aumento la frequenza di 1
+                }
+            } while (carattere != 13); //ripeto fin quando l'utente non preme invio (cr)
+
+           // Console.WriteLine("Hai premuto invio.");
+            Console.WriteLine("\nQuesta è la frequenza dei caratteri: ");
+
+            for(int i=0; i < frequenza.Length; i++) //ciclo for per stampare i caratteri con la loro frequenza
+            {
+                if(frequenza[i] != 0) //li stampo solo se l'utente li ha inseriti almeno 1 volta
+                {
+                    Console.WriteLine($"{tabellaAscii[i]} : {frequenza[i]} volte");
+                }
+            }
+            Console.ReadLine(); //per resettare line feed
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ PrendiParola/PrendiParola/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prendiParola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int maxParole = 3;
+            char tasto;
+            int contParole = 0;
+            string numeri = "", parola = "";
+            bool trovato = false;
+            string[] dizionario = new string[maxParole];
+            Console.WriteLine("inserire sequenza di caratteri");
+            do
+            {
+                tasto = Convert.ToChar(Console.Read());
+                if (Char.IsLetter(tasto))
+                {
+                    parola = parola + Convert.ToChar(tasto);
+                }
+                else if (tasto == ' ' || tasto == 13)
+                // prendo una parola quando trovo uno spazio o cr
+                {
+                    if (contParole == maxParole)
+                    {
+                        Console.WriteLine("Dizionario pieno");
+                    }
+                    else
+                    {
+                        trovato = false;
+                        for (int i = 0; i < contParole && !trovato; i++)
+                        {
+                            trovato = parola == dizionario[i];
+                        }
+                        if (!trovato)
+                        {
+                            dizionario[contParole] = parola;
+                            contParole++;
+                        }
+
+                    }
+                    parola = "";
+                }
+
+            } while (tasto != 13);
+
+            for (int i = 0; i < contParole; i++)
+            {
+                Console.WriteLine(dizionario[i]);
+            }
+            Console.ReadLine(); // salta line feed
+            Console.ReadLine(); // serve per bloccare programma
+
+        }
+    }
+}   // la mamma va al mercato
+    // la 
+    // mamma
+    // va
+    // al
+    // mercato
